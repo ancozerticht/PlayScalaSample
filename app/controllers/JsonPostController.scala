9b5b97ce5b73eed14c7f5b77c9707922b2c9e23f
ancozerticht <ancovorbish9oita180rz@gmail.com>
2021-09-01 20:16:38 +0900

    SupressWarningsを外した

--- app/controllers/JsonPostController.scala
+++ app/controllers/JsonPostController.scala
@@ -8,10 +8,8 @@ import javax.inject.Inject
 
 class JsonPostController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   case class RequestRecordModel(id: Int, name: String)
-  @SuppressWarnings(Array("org.wartremover.warts.Nothing", "org.wartremover.warts.Any"))
   private implicit val recordReads: Reads[RequestRecordModel] = Json.reads[RequestRecordModel]
 
-  @SuppressWarnings(Array("org.wartremover.warts.Any", "org.wartremover.warts.ToString"))
   def post(): Action[JsValue] = Action(parse.json) { implicit request =>
     val body = request.body.validate[RequestRecordModel]
     body.fold(
